e01f6e99a7a5e3d14b0643eaec6065186bb9c0f7
NeoAtlantis <contact@chaobai.li>
2024-06-18 19:36:04 +0200

    Use owner hierarchy.
    We tie volatile hmac keys to a owner hierarchy, so that the primary keys should not needs to be generated each time, and that once the primary key is fixed, we may use encrypted hmac_key.pub & hmac_key.priv, rather than plain HMAC secrets, to restore HMAC state over power cycles without worrying about bus sniffing.

--- demo.sh
+++ demo.sh
@@ -12,22 +12,26 @@ tpm2 startup -c
 echo "Test with 10 random bytes..."
 tpm2 getrandom --hex 10
 
+# If we have got a copy of the primary key.
 if [ ! -f ./primary.ctx ]; then
     echo "Create primary key..."
-    tpm2 createprimary --hierarchy n -c primary.ctx
+    tpm2 createprimary --hierarchy o -c primary.ctx
+    # should not be on NULL hierarchy, as we want to reload the key to same primary key.
     tpm2 flushcontext -t
 fi
 
-if [ ! -f ./hmac.ctx ]; then
+# If we have previously imported the key.
+if [ ! -f ./hmac_key.priv ] || [ ! -f ./hmac_key.pub ]; then
     tpm2 flushcontext -t
-
-    tpm2 import -C primary.ctx -G hmac -r hmac_key.priv -u hmac_key.pub -i test.bin
-    tpm2 load -C primary.ctx -u hmac_key.pub -r hmac_key.priv -c hmac.ctx
-    tpm2 flushcontext -t
+    tpm2 import -C primary.ctx -G hmac -r hmac_key.priv -u hmac_key.pub -i test.bin -a "stclear"
 fi
 
-echo "Import done, now lets do some test."
+# Always reload the key and its context...hmac.ctx is volatile upon each flushcontext or reset or resume.
+tpm2 load -C primary.ctx -u hmac_key.pub -r hmac_key.priv -c hmac.ctx
 cat test.bin | tpm2 hmac -c hmac.ctx --hex
 
+echo "Import done, now lets do some test."
+
+# Now to backup: primary.ctx, hmac_key.*
 
 printf "\n...done\n"
